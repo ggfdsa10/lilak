8d06375d742e30abf771d00af36ceca74990f800
ggfdsa10 <zxzfdsa@gmail.com>
2026-06-10 09:03:40 +0900

    test si mc

--- tpcdrum/simulation/makeSimData.C
+++ tpcdrum/simulation/makeSimData.C
@@ -3,14 +3,20 @@ void makeSimData()
     auto run = new LKRun();
     run -> AddDetector(new TPCDrum());
     // run -> SetInputFile("./simTest.root");
-    run -> SetInputFile("./mc_direct_trigger_all.root");
+    run -> SetInputFile("./test.root");
+    // run -> SetInputFile("/home/shlee/workspace/lilak/data/run_0000.root");
 
-    run -> Add(new STDDriftElectronMaker);
+    // run -> Add(new STDDriftElectronMaker); // TPC reponse
+    run -> Add(new STDSiResponseMaker);   // Si detector response
+
     run -> Add(new STDElectronicsMaker);
-    run -> Add(new STDNoiseSubtractor);
-    run -> Add(new STDPulseAnalyser);
-    run -> Add(new STDMCViewer);
+
+    // STDNoiseSubtractor* subtract = new STDNoiseSubtractor();
+    // subtract -> DrawRawADCPad();
+    // run -> Add(subtract);
+    // run -> Add(new STDPulseAnalyser);
+    // run -> Add(new STDMCViewer);
 
     run -> Init();
-    run -> Run(30);
+    run -> Run();
 }
